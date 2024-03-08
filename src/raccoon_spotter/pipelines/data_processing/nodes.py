f6f16c44e0fafdc29e0948514dce829154f88fb2
Clark <clark0712@foxmail.com>
2024-03-08 22:00:18 +0100

    feat: Add image arrays reshape & resize node

--- src/raccoon_spotter/pipelines/data_processing/nodes.py
+++ src/raccoon_spotter/pipelines/data_processing/nodes.py
@@ -1,19 +1,47 @@
+from typing import Dict
+
 import cv2
 import numpy as np
 
 
-def reshape_image_arrays(image_arrays: np.ndarray) -> np.ndarray:
+def reshape_image_arrays(image_arrays: np.ndarray) -> Dict[str, np.ndarray]:
+    """
+    Add a color channel for greyscale images.
+
+    Parameters:
+    - image_arrays (np.ndarray): A numpy array containing image arrays, x for images, y for labels.
+
+    Returns:
+    - reshaped_dict (str, np.ndarray): A dictionary containing x (array of images) and y (labels).
+    """
     GRAYSCALE_CHANNELS = 2
+    RGB_CHANNELS = 3
     reshaped_arrays = []
-    for image_array in image_arrays:
-        if len(image_array.shape) == GRAYSCALE_CHANNELS:
-            image_gray2rgb = cv2.cvtColor(image_array, cv2.COLOR_GRAY2RGB)
-            reshaped_arrays.append(image_gray2rgb)
+    for img_array in image_arrays["x"]:
+        if len(img_array.shape) == GRAYSCALE_CHANNELS:
+            converted_array = cv2.cvtColor(img_array, cv2.COLOR_GRAY2RGB)
+            reshaped_arrays.append(converted_array)
+        elif len(img_array.shape) != RGB_CHANNELS:
+            raise ValueError(
+                f"Image array does not have three dimensions: {img_array.shape}"
+            )
         else:
-            reshaped_arrays.append(image_array)
-    reshaped_array = np.array(reshaped_arrays, dtype=object)
-    return reshaped_array
+            reshaped_arrays.append(img_array)
+
+    return {
+        "x": np.array(reshaped_arrays, dtype=object),
+        "y": image_arrays["y"],
+    }
 
 
-def resize_image_arrays(reshaped_array: np.ndarray):
-    pass
+def resize_image_arrays(
+    resize_image_config: dict, image_array: np.ndarray
+) -> Dict[str, np.ndarray]:
+    resized_arrays = []
+    target_width = resize_image_config["target_width"]
+    target_height = resize_image_config["target_height"]
+    for img in image_array["x"]:
+        resized_img = cv2.resize(img, (target_width, target_height))
+        # resized_img = resized_img.astype(float)
+        resized_arrays.append(resized_img)
+    return {"x": np.stack(resized_arrays, axis=0), "y": image_array["y"]}

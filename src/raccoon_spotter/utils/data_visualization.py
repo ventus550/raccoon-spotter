642cb5a3f784cfdbfdda82656217881a6cd6a3b0
Clark <clark0712@foxmail.com>
2024-03-08 12:43:30 +0100

    feat: add a tool for drawing bounding boxes on image arrays(#6)

--- src/raccoon_spotter/utils/data_visualization.py
+++ src/raccoon_spotter/utils/data_visualization.py
@@ -1,22 +1,5 @@
 import cv2
 import numpy as np
-from kedro.config import OmegaConfigLoader
-
-
-def verify_bounding_box(index):
-    catalog_conf = OmegaConfigLoader(conf_source=".")["catalog"]
-    path = catalog_conf["raccoon_data_array"]["filepath"]
-    data_arr = np.load(path, allow_pickle=True)
-    image_array = data_arr["x"][index]
-    bounding_box = data_arr["y"][index]
-    img = cv2.rectangle(
-        image_array,
-        (bounding_box[0], bounding_box[2]),
-        (bounding_box[1], bounding_box[3]),
-        (255, 0, 0),
-        2,
-    )
-    return img
 
 
 def draw_bounding_box(image_array: np.ndarray, bounding_box: np.ndarray) -> np.ndarray:
@@ -24,7 +7,7 @@ def draw_bounding_box(image_array: np.ndarray, bounding_box: np.ndarray) -> np.n
     Draw bounding boxes on the image.
 
     Parameters:
-    - img (np.ndarray): The image array.
+    - image_array (np.ndarray): The image array.
     - bounding_boxes (np.ndarray): xmin, xmax, ymin, ymax.
 
     Returns:

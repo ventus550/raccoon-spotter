477be025e5094e54de5b2dd4d9e3f6b5912291d4
ventus <ventus@zephyr.home>
2024-05-26 11:30:45 +0200

    fix: config resolutions

--- src/raccoon_spotter/utils/configs.py
+++ src/raccoon_spotter/utils/configs.py
@@ -1,7 +1,13 @@
-from kedro.config import OmegaConfigLoader
+from kedro.config import MissingConfigException, OmegaConfigLoader
 
 loader = OmegaConfigLoader("conf")
 
-configs = OmegaConfigLoader("conf", config_patterns={"configs": ["**/configs.yml"]})[
-    "configs"
-]
+
+try:
+    configs = OmegaConfigLoader(
+        "conf/local", config_patterns={"configs": ["**/configs.yml"]}
+    )["configs"]
+except MissingConfigException:
+    configs = OmegaConfigLoader(
+        "conf/base", config_patterns={"configs": ["**/configs.yml"]}
+    )["configs"]
